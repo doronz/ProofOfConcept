1bc4ae4a8a156c386c13096f4593556e936562d1
Doron <devdoron@gmail.com>
2015-01-23 15:12:32 -0800

    Initial commit trying to get gradle to work.

---
+++ android/src/com/doronzehavi/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.doronzehavi.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.doronzehavi.ProofOfConcept;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProofOfConcept(), config);
+	}
+}
