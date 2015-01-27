821e515fe111606c142a04d6be4ee1d5b47a566c
Doron <devdoron@gmail.com>
2015-01-27 13:29:05 -0800

    Started documenting the code to better explain what each part does.

--- android/src/com/doronzehavi/android/AndroidLauncher.java
+++ android/src/com/doronzehavi/android/AndroidLauncher.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.doronzehavi.ProofOfConcept;
 
+/**
+ * This class is a "starter class". It is used specifically to start the game on an Android device.
+ */
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
--- core/src/com/doronzehavi/ProofOfConcept.java
+++ core/src/com/doronzehavi/ProofOfConcept.java
@@ -9,15 +9,53 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class ProofOfConcept extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
-	public void create () {
+
+    /**
+     * This is called when the application is ready and we can start loading our assets and create stage and actors.
+     */
+    @Override
+    public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
-	@Override
-	public void render () {
+    /**
+     * This is called every time the drawable surface is resized.
+     */
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    /**
+     * Called when the application goes into the background.
+     */
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    /**
+     * Called when the application returns from a pause.
+     */
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    /**
+     * Called when the game is done and application is closed. Cleanup occurs here.
+     */
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    /**
+     * This is the infinite game loop, called until game is over.
+     */
+    @Override
+    public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
